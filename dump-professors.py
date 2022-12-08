12fa867d4ee067637813172f9af1c8e3b0793f05
mfish33 <32677537+mfish33@users.noreply.github.com>
2022-12-08 15:31:48 -0800

    Update in preperation for trpc

--- dump-professors.py
+++ dump-professors.py
@@ -4,36 +4,36 @@ import os
 import multiprocessing as mp
 import json
 
-POLYRATINGS_API_BASE = 'https://api-prod.polyratings.dev/'
+POLYRATINGS_API_BASE = 'https://api-prod.polyratings.org/'
 
 # arbitrary chunk size, this should be adjusted based on the performance of the workflow runner
-CHUNK_SIZE = 200
+CHUNK_SIZE = 100
 
 def get_professors(id_list):
     print(f'Current ids: {id_list}')
     try:
         resp = requests.post(
-                POLYRATINGS_API_BASE + f'admin/bulk/professors', json={'keys':id_list},
+                POLYRATINGS_API_BASE + f'admin.getBulkValues', json={'bulkKey': "professors",'keys':id_list},
                 headers={'Authorization': f'Bearer {auth_token}'},
                 timeout=8
         )
     # sometimes the request to Cloudflare hangs, so we put a timeout to re-fire the request
     except requests.exceptions.ReadTimeout as _:
         resp = requests.post(
-                POLYRATINGS_API_BASE + f'admin/bulk/professors',
-                json={'keys':id_list}, headers={'Authorization': f'Bearer {auth_token}'},
+                POLYRATINGS_API_BASE + f'admin.getBulkValues',
+                json={'bulkKey': "professors",'keys':id_list}, headers={'Authorization': f'Bearer {auth_token}'},
                 timeout=14
         )
 
     if resp.status_code != 200:
         raise RuntimeError(f'Response from API for professor {id} was not 200: {resp.status_code} and text {resp.text}')
 
-    return resp.json()
+    return resp.json()['result']['data']
 
 
 if __name__ == '__main__':
     response = requests.post(
-            POLYRATINGS_API_BASE + 'login',
+            POLYRATINGS_API_BASE + 'auth.login',
             json={
                 'username': os.environ['POLYRATINGS_USERNAME'],
                 'password': os.environ['POLYRATINGS_PASSWORD']
@@ -42,10 +42,10 @@ if __name__ == '__main__':
     if response.status_code != 200:
         raise RuntimeError(f'Response from API was not 200: {response.status_code}')
 
-    auth_token = response.json()['accessToken']
+    auth_token = response.json()['result']['data']
 
     response = requests.get(
-            POLYRATINGS_API_BASE + 'admin/bulk/professors',
+            POLYRATINGS_API_BASE + 'admin.getBulkKeys?input=%22professors%22',
             headers={'Authorization': f'Bearer {auth_token}'}
     )
 
@@ -53,7 +53,7 @@ if __name__ == '__main__':
         raise RuntimeError(f'Response from API was not 200: {response.status_code}')
 
 
-    ids = response.json()
+    ids = response.json()['result']['data']
     num_processes = max(1, os.cpu_count() - 1)
 
     try:
@@ -66,14 +66,14 @@ if __name__ == '__main__':
 
     # flatten all of the responses and make sure that we don't include the truncated prof list
     professors = [prof for sublist in professors for prof in sublist if not isinstance(prof, list)]
+    professors = sorted(professors, key=lambda p: p['id']) 
 
     with open('professor-dump.json', 'w') as f:
-        json.dump(professors, f)
+        json.dump(professors, f, indent=2)
 
     # truncated prof list is just professors without reviews
     for p in professors:
         p.pop('reviews')
 
     with open('professor-list.json', 'w') as f:
-        json.dump(professors, f)
-
+        json.dump(professors, f,  indent=2)
